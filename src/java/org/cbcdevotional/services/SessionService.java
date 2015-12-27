ad000c6d406ab62ddfe2081387b75741afeb1678
arista <arista@abramsons.info>
2015-12-27 16:32:36 -0500

    started on an ant/ivy-based build system

---
+++ src/java/org/cbcdevotional/services/SessionService.java
@@ -0,0 +1,5 @@
+package org.cbcdevotional.services;
+
+public class SessionService
+{
+}
---
+++ test/java/org/cbcdevotional/services/test/SessionServiceTest.java
@@ -0,0 +1,13 @@
+package org.cbcdevotional.services.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class SessionServiceTest
+{
+  @Test
+  public void testExample()
+  {
+    assertTrue(3 == 3);
+  }
+}
